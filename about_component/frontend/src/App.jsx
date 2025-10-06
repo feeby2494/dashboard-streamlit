22f813a444a24231f57b42b260a079539653f67f
Jamie Lynn <toby2494@hotmail.com>
2025-10-05 21:58:10 -0500

    Home and about components are changed to desc their purpose

--- about_component/frontend/src/App.jsx
+++ about_component/frontend/src/App.jsx
@@ -17,26 +17,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>About App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This is Seolynn Streamlit Project
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
